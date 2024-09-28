5974bca1ac56b52a0a4f812b1dd7b6921b373856
glomukabwa <gloria.mukabwa@strathmore.edu>
2024-09-28 17:22:54 +0300

    Constants for database connection

---
+++ includes/constants.php
@@ -0,0 +1,25 @@
+<?php
+    //Creating constants
+    define('DBTYPE', 'PDO');
+    define('HOSTNAME', '127.0.0.1');
+    define('DBPORT', '3306');
+    define('HOSTUSER', 'root');
+    define('HOSTPASS', '');
+    define('DBNAME', 'api_d');
+
+    
+   /*print date("l"); //This will print the current day of the week
+
+   if(date("l") == "Monday"){
+    print "Yes";
+   }
+
+   //OR
+
+   switch(date("l")){
+    case 'Friday':
+        print "Yes";
+    break;
+    case 'Monday':
+        print 'No';
+   }*/
