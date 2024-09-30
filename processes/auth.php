164592c45b20434c36885ab857110299facd05cf
glomukabwa <gloria.mukabwa@strathmore.edu>
2024-09-30 19:50:08 +0300

    Verifying full name input

--- processes/auth.php
+++ processes/auth.php
@@ -8,7 +8,15 @@ class auth{
             $fullname = $_SESSION["fullname"] = $conn->escape_values(ucwords(strtolower($_POST["fullname"])));
             $email_address = $_SESSION["email_address"] = $conn->escape_values(strtolower($_POST["email_address"]));
             $username = $_SESSION["username"] = $conn->escape_values(strtolower($_POST["username"]));
-            
+
+// Implement input validation and error handling
+// =============================================
+// Sanitize all inputs
+
+// verify that the fullname has only letters, space, dash, quotation
+if(ctype_alpha(str_replace(" ", "", str_replace("\'", "", $fullname))) === FALSE){
+    $errors['nameLetters_err'] = "Invalid name format: Full name must contain letters and spaces only etc " . $fullname;
+}
 
         }
     }
